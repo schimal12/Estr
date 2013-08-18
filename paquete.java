02c9a632f96ad6375f1dff7eceaaa256d35c355d
Jesús Chimal-Monroy <schimal12@gmail.com>
2013-08-18 00:26:55 -0500

    Second commit

---
+++ paquete.java
@@ -0,0 +1,12 @@
+public class paquete {
+	private evento []eventos;
+	private int cont_;
+	public paquete(int numEventos){
+		eventos=new evento[numEventos];
+		cont_=0;
+	}
+	public void agregarEvento(String nombre, String descripcion, String lugar){
+		evento e1 = new evento(nombre, descripcion, lugar);
+		eventos[cont_]=e1;
+		cont_++;
+	}
