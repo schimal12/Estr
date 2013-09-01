46e9d1cb97425ec07e343384cfbdd76a2574ec1c
schimal12 <schimal12@gmail.com>
2013-08-31 23:09:25 -0500

    Update Estudiantes.java

--- Tarea3/LaChenchis/Estudiantes.java
+++ Tarea3/LaChenchis/Estudiantes.java
@@ -3,10 +3,13 @@ public class Estudiantes {
 	private String nombre;
 	private String clavemateria;
 	private Materias [] mat;
-	public Alumno(String carr, String nom, String CM){
+	Private int cantidad;
+	public Alumno(String carr, String nom, String CM, int cant){
 		this.carrera=carr;
 		this.nombre=nom;
 		this.clavemateria=CM;
+		this.cantidad=cant;
+		mat=new Materia[cant];
 		
 	}
 
