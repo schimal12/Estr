fc9cf78de6c1d61a9ece4ccd04a1468e65a25d36
schimal12 <schimal12@gmail.com>
2013-08-31 23:36:06 -0500

    Update Materias.java

--- Tarea3/LaChenchis/Materias.java
+++ Tarea3/LaChenchis/Materias.java
@@ -1,16 +1,21 @@
 public class Materia {
 	private String clave;
 	private String nombre;
-	privateint calificacion;
+	private int calificacion;
 	public Materia(String cl, String no, int calif){
 		this.clave=cl;
 		this.nombre=no;
 		this.calificacion=calif;
 	}
 	public int getClave(){
-		return clave;
+		return cl;
+	}
+	public int getNombre(){
+		return no;
 	}
 
-	
+	public int getCalificacion(){
+		return calif;
+	}
 
 }
