a5e50f11406b5576444c25ede074ed65a272d512
schimal12 <schimal12@gmail.com>
2013-08-31 23:20:43 -0500

    Update Estudiantes.java

--- Tarea3/LaChenchis/Estudiantes.java
+++ Tarea3/LaChenchis/Estudiantes.java
@@ -1,32 +1,28 @@
 public class Estudiantes {
 	private String carrera;
 	private String nombre;
-	private Materias [] mat;
-	private int cantidad;
-	public Alumno(String carr, String nom, int cant){
-		this.carrera=carr;
-		this.nombre=nom;
-		this.cantidad=cant;
+	private int numeroMaterias;
+	private Materias [ ] mat;
+	private int contador;
+	public Alumno(String carrera, String nombre, String numeroMaterias){
+		this.carrera=carrera;
+		this.nombre=nombre;
+		this.numeroMaterias=numeroMaterias;
 		mat=new Materia[cant];
+		this.contador=0;
 		
 	}
-	public String getCarrera() {
+	public int getnumeroMaterias(){
+		return numeroMaterias;
+	}
+	public int getMaterias(){
+		return mat;
+	}
+	public String getNombre(){
+		return nombre
+	}
+	public String getCarrera(){
 		return carrera;
-	}
-	public void setCarrera(String carr) {
-		this.carrera = carr;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nom) {
-		this.nombre = nom;
-	}
-	public String getCantidad(){
-		return cantidad;
-	}
-	public void setCantidad(String cant){
-		this.cantidad=cant;
 	}
 
 }
