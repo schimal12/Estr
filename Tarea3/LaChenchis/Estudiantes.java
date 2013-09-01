766e02a20b0b1708b7480fec48a7def6f0a104ba
schimal12 <schimal12@gmail.com>
2013-09-01 17:43:12 -0500

    Update Estudiantes.java

--- Tarea3/LaChenchis/Estudiantes.java
+++ Tarea3/LaChenchis/Estudiantes.java
@@ -1,46 +1,68 @@
-//Falta ponerle la interfaz comparable 
-public class Estudiante {
-	private String carrera;
-	private String nombre;
-	private int numeroMaterias;
-	private Materias [ ] mat;
-	private int contador;
-	public Alumno(String carrera, String nombre, String numeroMaterias){
-		this.carrera=carrera;
-		this.nombre=nombre;
-		this.numeroMaterias=numeroMaterias;
-		mat=new Materia[cant];
-		this.contador=0;
-		
-	}
-	public int getnumeroMaterias(){
-		return numeroMaterias;
-	}
-	public int getMaterias(){
-		return mat;
-	}
-	public String getNombre(){
-		return nombre
-	}
-	public String getCarrera(){
-		return carrera;
-	}
-	//Calculamos el promedio
-	public double promedio(){
-		double promedioAux=0;
-		double pF;
-		for(int i=0; i<numeroMaterias;i++){
-			promedioAux += this.getMaterias()[i].getCalificacion();
-		}
-	 
-		pF=promedioAux/numeroMaterias;
-			return pF;
-	}
-	//Agregamos por composición materias 
-	public void agregarMaterias(String clave, String nombre, int calificacion){
-		Materia m1= new Materia(clave, nombre, calificacion);
-		mat[contador]=m1;
-		contador++;
-	}
-	
+
+public class Estudiante implements Comparable<Estudiante>{
+    private String carrera;
+    private String nombre;
+    private int numeroMaterias;
+    private Materia [ ] mat;
+    private int contador;
+    private int cant;
+    public Estudiante(String carrera, String nombre, int numeroMaterias, int contador, int cant){
+        this.carrera=carrera;
+        this.nombre=nombre;
+        this.numeroMaterias=numeroMaterias;
+        mat=new Materia[cant];
+        this.contador=0;
+        
+    }
+    public int getnumeroMaterias(){
+        return numeroMaterias;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public Materia [ ] getMaterias(){
+         return mat;
+    }
+    public String getCarrera(){
+        return carrera;
+    }
+    public double promedio(){
+        double promedioAux=0;
+        double pF;
+        for(int i=0; i<numeroMaterias;i++){
+            promedioAux += this.getMaterias()[i].getCalificacion();
+            }
+        pF=promedioAux/numeroMaterias;
+            return pF;
+    }
+    public double promedio(Estudiante estu){
+        double promedioAux=0;
+        double pF;
+        for(int i=0; i<numeroMaterias;i++){
+            promedioAux += estu.getMaterias()[i].getCalificacion();
+            }
+        pF=promedioAux/numeroMaterias;
+            return pF; 
+    }
+    public void agregarMaterias(String clave, String nombre, int calificacion){
+        Materia m1= new Materia(clave, nombre, calificacion);
+        mat[contador]=m1;
+        contador++;
+    }
+    public int compareTo(Estudiante estu) {
+        double proAUX = 0;
+        for(int i=0;i<estu.getMaterias().length;i++){
+            proAUX+=(estu.getMaterias()[i].getCalificacion());
+           
+        }
+        proAUX=(proAUX/estu.getMaterias().length);
+        if(this.promedio()==proAUX){
+            return (int) (this.promedio()-proAUX);
+        }else{
+            if(this.promedio()>proAUX){
+                return (int) (this.promedio()-proAUX);
+            }
+            return (int) (this.promedio()-proAUX);
+        }
+    }
 }
