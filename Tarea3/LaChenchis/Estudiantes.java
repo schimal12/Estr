d75e086e8a78396097de13edd105f63eef693ad2
schimal12 <schimal12@gmail.com>
2013-08-31 23:00:22 -0500

    Update Estudiantes.java

--- Tarea3/LaChenchis/Estudiantes.java
+++ Tarea3/LaChenchis/Estudiantes.java
@@ -1,2 +1,14 @@
-// Aquí haremos la clase estudiante 
 
+public class Estudiantes {
+	protected String carrera;
+	protected String nombre;
+	protected String clavemateria;
+	
+	public Alumno(String carr, String nom, String CM){
+		this.carrera=carr;
+		this.nombre=nom;
+		this.clavemateria=CM;
+		
+	}
+
+}
